f136b414d67abce2316845a57bb3bb8008275ed0
Jack Carmichael <carmichaeljr@outlook.com>
2023-04-15 11:51:20 -0600

    added terminal shortcut;added error diagnositc popup

--- lua/carmichaeljr/remap.lua
+++ lua/carmichaeljr/remap.lua
@@ -1,4 +1,10 @@
 vim.keymap.set("n","<leader>fe",vim.cmd.Ex)
 
+vim.keymap.set("t","<esc>","<C-\\><C-n>")
+
 vim.keymap.set("v","J",":m '>+1<CR>gv=gv'")
 vim.keymap.set("v","K",":m '<-2<CR>gv=gv'")
+
+vim.keymap.set("n","<leader>e",function()
+    vim.diagnostic.open_float(0,{scope="line"})
+end)
